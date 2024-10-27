bb8c98ac40013617293a2defab35fe4c5b9e562c
Blake Collins <thekidd2211@gmail.com>
2024-10-27 13:53:29 +1300

    Comments

--- core/src/main/java/io/github/spacecraft/AsteroidManager.java
+++ core/src/main/java/io/github/spacecraft/AsteroidManager.java
@@ -119,7 +119,8 @@ public class AsteroidManager {
         Array<Asteroid> validAsteroids = new Array<>();
         for (Asteroid asteroid : asteroids) {
             float asteroidY = asteroid.getSprite().getY();
-            if ((asteroidY < topBoundary && asteroidY > bottomBoundary)&&!asteroid.isHarvesting()) { // if asteroid is within boundaries
+            // if asteroid is within boundaries AND is not currently being harvested
+            if ((asteroidY < topBoundary && asteroidY > bottomBoundary)&&!asteroid.isHarvesting()) {
                 validAsteroids.add(asteroid);
             }
         }
@@ -144,7 +145,8 @@ public class AsteroidManager {
         Array<Asteroid> validAsteroids = new Array<>();
         for (Asteroid asteroid : asteroids) {
             float asteroidY = asteroid.getSprite().getY();
-            if ((asteroidY < topBoundary && asteroidY > bottomBoundary)&&!asteroid.isHarvesting()) { // if asteroid is within boundaries
+            // if asteroid is within boundaries AND is not currently being harvested
+            if ((asteroidY < topBoundary && asteroidY > bottomBoundary)&&!asteroid.isHarvesting()) {
                 validAsteroids.add(asteroid);
             }
         }
